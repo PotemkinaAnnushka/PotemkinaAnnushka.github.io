62ac78555ba1fc0c71c1dd17cfa4c559fc7b5417
Annushka <138501646+PotemkinaAnnushka@users.noreply.github.com>
2023-07-04 15:01:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+gsap.from ('.greetingThree', {
+    y: -200,
+    opacity: 0,
+    duration: 0.7,
+    delay: 1
+});
+gsap.from ('.greetingFour', {
+    opacity: 0,
+    duration: 0.2,
+    stagger: 0.2,
+    delay: 2
+})
+gsap.from ('.aboutUsChefPic', {
+    x: 500,
+    opacity: 0,
+    duration: 1,
+    delay:2.5
+});
+gsap.from ('.khinkaliOne', {
+    opacity:0,
+    scale: -1, 
+    duration: 2.5, 
+    ease: 'elastic', 
+    delay: 2.8
+});
+gsap.from ('.khinkaliTwo', {
+    opacity:0,
+    scale: -1, 
+    duration: 2.5, 
+    ease: 'elastic', 
+    delay: 3.2
+});
+gsap.from ('.khinkaliThree', {
+    opacity:0,
+    scale: -1, 
+    duration: 2.5, 
+    ease: 'elastic', 
+    delay: 3.6
+});
+
+
